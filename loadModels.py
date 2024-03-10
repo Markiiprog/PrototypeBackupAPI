3ba98eaf8d9c712eaeb996236c6e24aba39f6a05
MarkP <markexequielpascua@gmail.com>
2024-03-10 09:32:30 +0800

    true gpu

--- loadModels.py
+++ loadModels.py
@@ -8,12 +8,12 @@ from speechbrain.pretrained import EncoderDecoderASR
 def OCR_Model():
 
     # Create an OCR reader with the specified language (Pretrained model) 
-    #reader =  easyocr.Reader(['en'],gpu=True) 
+    reader =  easyocr.Reader(['en'],gpu=True) 
 
     # Create an OCR reader with the specified language (custom recognition model)
     custom_model_path = 'ocr_finetuned'
 
-    reader =  easyocr.Reader(['en'],recog_network='fine_tuned_recognition',model_storage_directory=custom_model_path,user_network_directory=custom_model_path,gpu=False) 
+    reader =  easyocr.Reader(['en'],recog_network='fine_tuned_recognition',model_storage_directory=custom_model_path,user_network_directory=custom_model_path,gpu=True) 
 
     return reader
 
